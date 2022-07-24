3b62e199cf5431e3293f9d4efbefe79e9fc53993
IDAGIZA <ruthidagiza@gmail.com>
2022-07-25 01:48:59 +0300

    commit tree 3635a5bb6f1ea334e277b9513c83e2fa54c70bfd

--- src/data/items.js
+++ src/data/items.js
@@ -1,9 +1,32 @@
+import React, {useState} from "react";
 const items = [
   { id: 1, name: "Yogurt", category: "Dairy" },
   { id: 2, name: "Pomegranate", category: "Produce" },
-  { id: 3, name: "Lettuce", category: "Produce" },
-  { id: 4, name: "String Cheese", category: "Dairy" },
+	//@@ -6,4 +7,27 @@ const items = [
   { id: 5, name: "Cookies", category: "Dessert" },
 ];
 
-export default items;
+import React, { useState } from "react";
+
+function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+
+  function handleAddToCartClick() {
+    setIsInCart((isInCart) => !isInCart);
+  }
+
+  return (
+    <li className={isInCart ? "in-cart" : ""}>
+      <span>{name}</span>
+      <span className="category">{category}</span>
+      <button
+        className={isInCart ? "remove" : "add"}
+        onClick={handleAddToCartClick}
+      >
+        {isInCart ? "Remove From" : "Add to"} Cart
+      </button>
+    </li>
+  );
+}
+
+export default Item;
